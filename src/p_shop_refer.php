bae1d36df9ec607de64758c5790e1f9cd84f715f
mai-higuchi <k16jk107@st.kyusan-u.ac.jp>
2018-12-13 17:16:18 +0900

    Update p_shop_refer.php
    口コミ追加

--- src/p_shop_refer.php
+++ src/p_shop_refer.php
@@ -5,13 +5,13 @@ $sname=$_GET['sname'];
 
 
 
-$sql="SELECT sid,sname,address,open,close,time,budget,holiday,uid,rpoint
-		From tb_shop natural left join tb_review
+$sql="SELECT sid,sname,address,open,close,time,budget,holiday,uid,rpoint,rid,comment,pid,uname
+		From tb_shop natural left join tb_review natural left join tb_user
 		WHERE sname='$sname'";
 $rs = mysql_query($sql, $conn);
 if (!$rs) die ('エラー: ' . mysql_error());
 
-echo '<table border=1>';
+
 
 $row = mysql_fetch_array($rs) ;
 echo '<h1>'.$sname;
@@ -27,21 +27,42 @@ while ($row) {
 	//$r  = $row['urole'];// ユーザ種別コード取得（数字）
 	$_SESSION['sid']   = $row['sid'];
 
-
-	echo '<tr>';
+	echo '<table border=1>';
+	//echo '<tr>';
 	//echo '<tr><td>' .'店舗名'.'</td><td>'. $row['sname'] . '</td></tr>'; // ユーザ種別コードを名称に変換（連想配列利用）
 	echo '<tr><td>' .'営業時間'.'</td><td>'. $row['open'] . '～'.$row['close'].'</td></tr>';
 	echo '<tr><td>' .'住所'.'</td><td>'. $row['address'] . '</td></tr>';
 	echo '<tr><td>' .'定休日'.'</td><td>'. $row['holiday'] . '</td></tr>';
 	echo '<tr><td>' .'平均予算'.'</td><td>'. $row['budget'] .'円'. '</td></tr>';
-	echo '<tr><td>' .'所要時間'.'</td><td>'. $row['time'] .'分'. '</td></tr>';
+	echo '<tr><td>' .'所要時間'.'</td><td>'.'徒歩'. $row['time'] .'分'. '</td></tr>';
+
 	//echo '<td>'  . '</td>';
-	echo '</tr>';
+	//echo '</tr>';
 	//$n++;
-	$row = mysql_fetch_array($rs);
-}
+
+
 
 echo '</table>';
+
+echo'<h2>メニュー</h2>';
+
+echo '<h2>口コミ</h2>';
+
+	echo '<b>'.$row['uname'].'</b>';
+	for($i=0;$i<5;$i++){
+		if($i<$row ['rpoint']){
+			echo '★';
+		}else{
+			echo '☆';
+		}
+	}
+
+	echo '<br>'.$row['comment'];
+	//echo '<br><a href="?do=p_review_detail&uid">もっと見る</a>';
+
+	$row = mysql_fetch_array($rs);
+
+}
 
 
 echo '
