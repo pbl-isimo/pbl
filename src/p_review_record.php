e5c6bf4335a466a91cc41ccfdc58e6a1327487b4
Yui OOISHI <k16jk026@st.kyusan-u.ac.jp>
2018-12-13 23:22:58 +0900

    Update p_review_record.php

--- src/p_review_record.php
+++ src/p_review_record.php
@@ -1,12 +1,10 @@
 <html>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <h2>口コミ登録</h2>
-<form action="?do=p_review_save" method="post">
+<form action="?do=p_review_save" method="post" enctype="multipart/form-data">
 <?php
 require_once ('src/db_inc.php');
-echo 'コメント<br>
-<textarea name="kanso" rows="10" cols="60"></textarea><br><br>
-
-	<select name="rpoint">
+echo '<select name="rpoint">
 	<option value=selected>評価点</option>
 	<option value="5">5</option>
 	<option value="4">4</option>
@@ -14,9 +12,21 @@ echo 'コメント<br>
 	<option value="2">2</option>
 	<option value="1">1</option>
 </select><br><br>
+
+コメント<br>
+<textarea name="kanso" rows="10" cols="60"></textarea><br><br>
 		';
 
-echo '<input type="submit" value="登録">';
+
+echo '写真1<input type="file" name="upfile" size="30"/>
+			<br>写真2<input type="file" name="upfile2" size="30"/>
+			<br>写真3<input type="file" name="upfile3" size="30"/>
+
+			<br><br>
+<input type="submit" value="登録"/>
+			<br>
+			';
+
 ?>
 </form>
-<html>
+</html>
