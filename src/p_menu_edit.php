fd89c1e24c03779afc62c05a384b22380be7aaac
Maya TAKATSU <45749621+k16jk074@users.noreply.github.com>
2018-12-13 15:38:27 +0900

    Update p_menu_edit.php
    URLから店の名前と、ログインしているユーザで条件をつけて、データベースに検索をかけています
    
    そのユーザが登録したメニューが複数ある場合、縦に並べるようにしています

--- src/p_menu_edit.php
+++ src/p_menu_edit.php
@@ -3,19 +3,33 @@ $sname=$_GET ['sname'];
 echo '<h1>'.$sname.'</h1>';
 ?>
 <h2>メニュー編集</h2>
-<form action="" >
-メニュー名　<input type="text" name="name" size="20" maxlength="10"><br>
-値段　　　　<input type="text" name="price" size="20" maxlength="10">円<br>
-説明　　　　<textarea name="kanso" rows="4" cols="40"></textarea><br>
 <?php
 require_once ('db_inc.php');
 $uid = $_SESSION ['uid'];
-//echo $uid;
-
-//$sql="INSERT INTO tb_menu VALUES (,'',,);";
+// echo $uid;
+$sql = "SELECT * FROM tb_menu
+		natural join tb_shop
+		where sname='$sname'
+		and uid='$uid'";
+// $sql="INSERT INTO tb_shop VALUES (,'',,);";
 $rs = mysql_query ( $sql, $conn );
-$row = mysql_fetch_array ( $rs );
-var_dump($row);
+//var_dump($row);
+//$row = mysql_fetch_array ( $rs );
+if (!empty($row)) {
+	$num = mysql_num_rows ( $rs );
+
+	echo 'メニュー名　　　　　　　値段　　　　　　　　　　　説明　　　<br>';
+	for($j = 1; $j <= $num; $j ++) {
+		$row = mysql_fetch_array ( $rs );
+		echo '<form>
+	 <input type="text" name="name"  size="20" maxlength="10" value="' . $row ['item'] . '">
+	 <input type="text" name="price" size="20" maxlength="10" value="' . $row ['price'] . '">円
+	 <textarea name="contents" rows="1" cols="40">' . $row ['mcontents'] . '</textarea><br>
+	 ';
+	}
+	echo '<input type="submit" value="登録">';
+}
+else{
+	echo 'まだ登録がありません';
+}
 ?>
-<input type="submit" value="登録">
-</form>
