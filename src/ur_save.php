ee0f1e96fcc85b3c5e582d5fc159853f09934043
Mitsuhiro Inoue <k16jk012@st.kyusan-u.ac.jp>
2018-12-14 08:58:30 +0900

    Update ur_save.php

--- src/ur_save.php
+++ src/ur_save.php
@@ -1,7 +1,9 @@
 <?php
 require_once ('db_inc.php');
 
+$uname0 = $_POST ['uname'];
 $u = $_SESSION ['uid'];
+$people = 0;
 
 $sql = "
   SELECT uname, psword FROM tb_user
@@ -12,20 +14,38 @@ $p = mysql_fetch_assoc ( $rs );
 if (! $rs)
 	die ( 'エラー: ' . mysql_error () );
 
+if ($_POST ['uname'] != $p ['uname']) {
+	$sql1 = "
+SELECT COUNT(*) AS people FROM tb_user
+WHERE uname = '$uname0'";
+	$rs1 = mysql_query ( $sql1 );
+	$userp = mysql_fetch_assoc ( $rs1 );
+	$people = $userp['people'];
+}
 
+if ($people >= 1) {
+	echo '<h3>エラー:アカウント名が重複しています</h3>';
+} else if($people == 0) {
+	if ($_POST ['uname'] == "") {
+		echo '<h3>エラー:アカウント名が入力されていません</h3>';
 
-if ($_POST ['pass0'] != $p ['psword']) {
-	echo '<h3>エラー：現在のパスワードが一致しないので登録できません</h3>';
-} else {
-	$pass1 = $_POST ['pass1'];
-	$pass2 = $_POST ['pass2'];
-	if ($pass1 == $pass2) {
-		$uname = $_POST ['uname'];
-		$sql2 = "UPDATE tb_user SET uname='{$uname}',psword='{$pass1}' WHERE uid='{$u}'";
-		mysql_query ( $sql2, $conn );
-		echo '<h3>アカウントが更新されました</h3>';
 	} else {
-		echo '<h3>エラー：新しいパスワードが一致しないので登録できません</h3>';
+		if ($_POST ['pass0'] != $p ['psword']) {
+			echo '<h3>エラー：現在のパスワードが一致しないので登録できません</h3>';
+//			echo $u['people'];
+		} else {
+			$pass1 = $_POST ['pass1'];
+			$pass2 = $_POST ['pass2'];
+			if ($pass1 == $pass2) {
+				$uname = $_POST ['uname'];
+				$sql2 = "UPDATE tb_user SET uname='$uname',psword='$pass1' WHERE uid='{$u}'";
+				mysql_query ( $sql2);
+				echo '<h3>アカウントが更新されました</h3>';
+			} else {
+				echo '<h3>エラー：新しいパスワードが一致しないので登録できません</h3>';
+			}
+		}
 	}
 }
+
 ?>
