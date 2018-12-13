c13d8c04a9b7d7a4e758129adfcf8c5b175390b4
Yui OOISHI <k16jk026@st.kyusan-u.ac.jp>
2018-12-13 21:57:16 +0900

    Update p_review_record.php

--- src/p_review_record.php
+++ src/p_review_record.php
@@ -1,10 +1,22 @@
+<html>
 <h2>口コミ登録</h2>
-<form action="?do=p_review_detail" method="post">
+<form action="?do=p_review_save" method="post">
 <?php
 require_once ('src/db_inc.php');
 echo 'コメント<br>
-<textarea name="kanso" rows="20" cols="80"></textarea><br>
-<input type="submit" value="登録">
-';
+<textarea name="kanso" rows="10" cols="60"></textarea><br><br>
+
+	<select name="rpoint">
+	<option value=selected>評価点</option>
+	<option value="5">5</option>
+	<option value="4">4</option>
+	<option value="3">3</option>
+	<option value="2">2</option>
+	<option value="1">1</option>
+</select><br><br>
+		';
+
+echo '<input type="submit" value="登録">';
 ?>
 </form>
+<html>
