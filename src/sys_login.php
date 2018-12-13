e4c5674ac182bce1180bae91b14bfdd9fffe1eea
Marina SHINOZAKI <45754394+k16rs065@users.noreply.github.com>
2018-12-13 09:37:37 +0900

    ログイン画面
    デザイン改善

--- src/sys_login.php
+++ src/sys_login.php
@@ -1,17 +1,19 @@
+﻿<body bgcolor="#FFDBC9">
 <form action="?do=sys_check" method="post" class="form">
 <table class="table table-hover">
 	<tr>
-		<td><input type="text" name="uid" class="form-control"
+		<td><input type="text" name="uid" class="form-control"placeholder="ユーザID"
 			style="position: absolute; left: 45%; top: 48%"></td>
 	</tr>
 	<tr>
-		<td><input type="password" name="psword" class="form-control"
+		<td><input type="password" name="psword" class="form-control"placeholder="パスワード"
 			style="position: absolute; left: 45%; top: 54%"></td>
 	</tr>
 </table>
 <input type="submit" value="　　ロ　グ　イ　ン　　" class="btn btn-primary"
 	style="position: absolute; left: 45%; top: 60%">
 </form>
+</body>
 <?php
 //echo $_SESSION['login'];
 /*
