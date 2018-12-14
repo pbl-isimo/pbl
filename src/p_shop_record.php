af1ff502d035ae18e5b5faee92144b3cdcbba9c6
mai-higuchi <k16jk107@st.kyusan-u.ac.jp>
2018-12-14 17:09:26 +0900

    Update p_shop_record.php
    必須入力の説明追加

--- src/p_shop_record.php
+++ src/p_shop_record.php
@@ -1,16 +1,20 @@
 <h2>店舗登録</h2>
-<form action="" method="">
-名前　　　<input type="text" name="namae" size="20" maxlength="10"><br>
-住所　　　<input type="text" name="namae" size="70" maxlength="50"><br>
-定休日　　<input type="text" name="namae" size="4" maxlength="2">曜日<br>
-営業時間　<input type="text" name="namae" size="4" maxlength="20">
-～<input type="text" name="namae" size="4" maxlength="20"><br>
-所要時間　徒歩　<input type="text" name="namae" size="4" maxlength="2">分<br>
-平均予算　<input type="text" name="namae" size="4" maxlength="5">円<br>
-福岡県	北九州市八幡東区	川淵町	3-9	川淵町プレシャス301
+＊がついているところは必須入力
+<form action="?do=p_shop_save" method="post">
+*名前　　　<input type="text" name="sname" size="20" maxlength="10"><br>
+*住所　　　<input type="text" name="address" size="70" maxlength="50"><br>
+定休日　　<input type="text" name="holiday" size="4" maxlength="2">曜日<br>
+営業時間　<input type="text" name="open" size="4" maxlength="20">
+～<input type="text" name="close" size="4" maxlength="20"><br>
+所要時間　徒歩　<input type="text" name="time" size="4" maxlength="2">分<br>
+平均予算　<input type="text" name="budget" size="4" maxlength="5">円<br>
+
 <?php
+
+
 require_once ('db_inc.php');
 $uid = $_SESSION ['uid'];
+
 //echo $uid;
 /*
 $sql="INSERT INTO `pbl`.`tb_shop`
