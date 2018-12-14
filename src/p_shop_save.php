9b63cff38b5db15e4780b04f231f1a53717b01d9
mai-higuchi <k16jk107@st.kyusan-u.ac.jp>
2018-12-14 17:45:49 +0900

    Update p_shop_save.php
    uidがデータベースに追加されるようになりました

--- src/p_shop_save.php
+++ src/p_shop_save.php
@@ -12,7 +12,7 @@ require_once('db_inc.php');
 $coon=mysql_connect("localhost","root");
 mysql_select_db("pbl",$conn);
 
-//$uid = $_SESSION ['uid'];
+$uid = $_SESSION ['uid'];
 //echo $uid;
 
 $sname=$_POST['sname'];
@@ -26,15 +26,18 @@ $close = $_POST['close'];
 $time = $_POST['time'];
 $budget = $_POST['budget'];
 $holiday = $_POST['holiday'];
-//$uid = $_POST['uid'];
+$uid = $_SESSION['uid'];
 
 if($sname==""){
 	echo "店舗名が入力されていません";
 }else if($address==""){
 	echo "住所が入力されていません";
 }else{
-	$sql = "INSERT INTO tb_shop (sname,address,open,close,time,budget,holiday) VALUES
-	('$sname','$address','$open','$close','$time','$budget','$holiday')";
+	//$sql = "INSERT INTO tb_shop (sname,address,open,close,time,budget,holiday,uid) VALUES
+	//('$sname','$address','$open','$close','$time','$budget','$holiday,'$uid')";
+
+	$sql="INSERT INTO `tb_shop`( `sname`, `address`, `open`, `close`, `time`, `budget`, `holiday`, `uid`)
+		VALUES ('$sname','$address','$open','$close','$time','$budget','$holiday','$uid')";
 	echo '<h3>登録されました</h3>';
 }
 
