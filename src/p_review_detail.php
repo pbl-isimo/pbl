b1caefd942cc12e1eb51103c935b54632b705bf8
Yui OOISHI <k16jk026@st.kyusan-u.ac.jp>
2018-12-14 14:21:13 +0900

    Update p_review_detail.php

--- src/p_review_detail.php
+++ src/p_review_detail.php
@@ -1,6 +1,34 @@
-<br><br>
+<h2>口コミ詳細</h2>
 <?php
+require_once ('db_inc.php');
+$sname=$_GET[''];
+
+
+$sql="SELECT sid,rid,rpoint,comment,uid,pid,uname
+From tb_review Natural left join tb_user Natural left join tb_shop
+WHERE sname='$sname'";
+$rs = mysql_query($sql, $conn);
+$row = mysql_fetch_array($rs);
+while($row){
+	echo '<b>'.$row['uname'].'</b>';
+	for($i=0;$i<5;$i++){
+		if($i<$row ['rpoint']){
+			echo '★';
+		}else{
+			echo '☆';
+		}
+	}
+
+	echo '<br>'.$row['comment'];
+
+
+}
+
+echo '<br>';
 echo "更新日時：";
 echo date("Y")."年".date("n")."月".date("j")."日　";
 echo date("G").":".date("i");
+
+echo '</table>'
+
 ?>
