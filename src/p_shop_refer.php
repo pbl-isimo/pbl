3914975bf13f39406943384bbfcedd3b00f2ed73
mai-higuchi <k16jk107@st.kyusan-u.ac.jp>
2018-12-14 23:42:27 +0900

    Update p_shop_refer.php
    店舗詳細画面遷移追加

--- src/p_shop_refer.php
+++ src/p_shop_refer.php
@@ -105,15 +105,15 @@ while($row){
 echo '
 <br>
 <br>
-<a href="?do=p_review_record">口コミ編集</a>
+<a href="?do=p_review_record' . $_GET ['sname'] . '">口コミ編集</a>
 <br>
-<a href="?do=p_review_detail">口コミ情報</a>
+<a href="?do=p_review_detail' . $_GET ['sname'] . '">口コミ情報</a>
 <br>
 <a href="?do=p_menu_edit&sname=' . $_GET ['sname'] . '">メニュー編集</a>
 <br>
 <a href="?do=p_menu_refer&sname=' . $_GET ['sname'] . '">メニュー登録</a>
 <br>
-<a href="?do=p_shop_edit">店舗編集</a>
+<a href="?do=p_shop_edit&sname='.$_GET['sname'].'">店舗編集</a>
 <br>
 <a href="?do=p_map"target="_blank">マップ参照</a>
 ';
