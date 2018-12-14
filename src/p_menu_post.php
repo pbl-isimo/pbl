9d2fe9dd03c9ca6afeca984f731d6c40249d1f87
Maya TAKATSU <45749621+k16jk074@users.noreply.github.com>
2018-12-15 01:11:30 +0900

    menu_referの実行用
    menu_referの実行用としてp_menu_postを追加しました

---
+++ src/p_menu_post.php
@@ -0,0 +1,38 @@
+﻿<?php
+//実行
+$sname = $_GET ['sname'];
+
+$sid = $_REQUEST['sid'];
+$name = $_REQUEST['name'];
+$price = $_REQUEST['price'];
+$contents = $_REQUEST['contents'];
+
+echo $sid;
+echo $name;
+echo $price;
+echo $contents.'<br>';
+
+require_once ('db_inc.php');
+$mid=1;
+while($mid<1000){
+$sql="SELECT count(*) FROM `tb_menu`
+WHERE sid='$sid' and mid='$mid'";
+$rs = mysql_query ( $sql, $conn );
+$num = mysql_fetch_array ( $rs );
+//echo $sql;
+//var_dump($num);
+	if($num['count(*)']==0){
+		echo 'あたり';
+		break;
+	}else{
+		echo 'ハズレ';
+		$mid++;
+	}
+}
+echo "メニューid".$mid;
+$sql1="INSERT INTO `tb_menu`(`sid`, `item`, `price`, `mid`, `mcontents`)
+		VALUES ('$sid','$name','$price','$mid','$contents')";
+$rs1 = mysql_query ( $sql1, $conn );
+header( "Location:?do=p_shop_refer&sname='$sname'&sid='$sid'") ;
+//header( "Location: https://blog.codecamp.jp/color_pattern" ) ;
+?>
