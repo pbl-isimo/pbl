326d9581b7bb42a0f52a8ab2e4d87312c925e8c4
Mitsuhiro Inoue <k16jk012@st.kyusan-u.ac.jp>
2018-12-14 19:08:28 +0900

    Update p_review_detail.php

--- src/p_review_detail.php
+++ src/p_review_detail.php
@@ -1,34 +1,42 @@
 <h2>口コミ詳細</h2>
 <?php
 require_once ('db_inc.php');
-$sname=$_GET[''];
+$sid = $_SESSION['sid'];
 
 
-$sql="SELECT sid,rid,rpoint,comment,uid,pid,uname
+$sql = "SELECT uname,rpoint,comment
 From tb_review Natural left join tb_user Natural left join tb_shop
-WHERE sname='$sname'";
-$rs = mysql_query($sql, $conn);
-$row = mysql_fetch_array($rs);
-while($row){
-	echo '<b>'.$row['uname'].'</b>';
-	for($i=0;$i<5;$i++){
-		if($i<$row ['rpoint']){
+WHERE sid='$sid'";
+
+$rs = mysql_query ( $sql );
+$row = mysql_fetch_array ( $rs );
+echo '<center>';
+echo '<table border=1>';
+echo '<tr><th>ユーザ名</th><th>評価</th><th>コメント</th></tr>';
+
+while ( $row ) {
+	echo '<tr>';
+	echo '<td>' . $row ['uname'] . '</td>';
+	echo '<td>';
+	for($i = 0; $i < 5; $i ++) {
+		if ($i < $row ['rpoint']) {
 			echo '★';
-		}else{
+		} else {
 			echo '☆';
 		}
 	}
-
-	echo '<br>'.$row['comment'];
-
-
+	echo '</td>';
+	echo '<td>' . $row ['comment'] . '</td>';
+	echo '</tr>';
+	$row = mysql_fetch_array($rs);
 }
-
+// echo '<br>'.$row['rpoint'];
+echo '</table>';
+echo '</center>';
 echo '<br>';
 echo "更新日時：";
-echo date("Y")."年".date("n")."月".date("j")."日　";
-echo date("G").":".date("i");
-
-echo '</table>'
+echo date ( "Y" ) . "年" . date ( "n" ) . "月" . date ( "j" ) . "日　";
+echo date ( "G" ) . ":" . date ( "i" );
 
 ?>
+
